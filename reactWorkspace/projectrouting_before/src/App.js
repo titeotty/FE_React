528bf6e6fec4acf3287e291262f7a322015b517a
HYEON <titeotty12@gmail.com>
2024-06-11 11:27:28 +0900

    0611

--- reactWorkspace/projectrouting_before/src/App.js
+++ reactWorkspace/projectrouting_before/src/App.js
@@ -5,15 +5,20 @@ import Main from "./component/Main";
 import Board from "./component/Board";
 import Product from "./component/Product";
 import NotFound from './component/NotFound';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-		<Header />
-		<Main />
-		<Board />
-		<Product />
-		<NotFound />
+		<BrowserRouter>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Main/>}></Route>
+				<Route path="/board/" element={<Board/>}></Route>
+				<Route path="/product/" element={<Product/>}></Route>
+				<Route path="*" element={<NotFound/>}></Route>
+			</Routes>
+		</BrowserRouter>
     </div>
   );
 }
--- reactWorkspace/projectrouting_before/src/component/Header.js
+++ reactWorkspace/projectrouting_before/src/component/Header.js
@@ -1,8 +1,12 @@
+import {Link} from 'react-router-dom';
 
 function Header() {
     return (
       <div>
         <h1>헤더입니다.</h1>
+        <Link to="/">메인</Link> |
+        <Link to="/board">게시판</Link> |
+        <Link to="/product/">상품</Link> |
       </div>
     );
   }
