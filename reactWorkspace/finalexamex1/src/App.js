8f0f99cdaa4623c1ed3a2ccde67a04bf4b7c6b2a
HYEON <titeotty12@gmail.com>
2024-06-11 14:22:56 +0900

    0611

--- reactWorkspace/finalexamex1/src/App.js
+++ reactWorkspace/finalexamex1/src/App.js
@@ -2,8 +2,33 @@ import './App.css';
 import Header from "./component/Header";
 import TodoEditor from "./component/TodoEditor";
 import TodoList from "./component/TodoList";
+import { useState } from "react";
 
 function App() {
+  const mockTodo = [
+    {
+      id : 0,
+      isDone : false,
+      content : "React 공부하기",
+      createdDate : new Date().getTime(),
+    },
+
+    {
+      id : 1,
+      isDone : false,
+      content : "빨널",
+      createdDate : new Date().getTime(),
+    },
+
+    {
+      id : 2,
+      isDone : false,
+      content : "노래",
+      createdDate : new Date().getTime(),
+    },
+  ]
+
+  const [todo, setTodo] = useState(mockTodo);
   return (
     <div className="App">
       <Header />
--- reactWorkspace/finalexamex1/src/component/TodoItem.js
+++ reactWorkspace/finalexamex1/src/component/TodoItem.js
@@ -7,10 +7,12 @@ const TodoItem = () => {
                 <input type="checkbox" />
             </div>
             <div className="title_col">할 일</div>
-            <div className="date_col">{new Date().toLocalDateString()}</div>
+            <div className="date_col">{new Date().toLocalDateString}</div>
             <div className="btn_col">
                 <button>삭제</button>
             </div>
         </div>
     )
 }
+
+export default TodoItem;
